c42ef680f30afd362bd81c35414dc11079b9dd3e
LinxInformatica <diegolepore01@gmail.com>
2023-12-30 19:12:46 -0300

    vercel

--- client/src/helpers/siteroutes.helper.js
+++ client/src/helpers/siteroutes.helper.js
@@ -1,21 +1,28 @@
 const SITEROUTES = {
-    URL: 'http://localhost:3001',
-    VIDEOGAMES:'http://localhost:3001/videogames',
-    GENRES:'http://localhost:3001/genres',
-    PLATFORMS:'http://localhost:3001/platforms',
-    SETUP:'http://localhost:3001/setup',
-    IMAGES:'http://localhost:3001/images/',
+    // URL: 'http://localhost:3001',
+    // VIDEOGAMES:'http://localhost:3001/videogames',
+    // GENRES:'http://localhost:3001/genres',
+    // PLATFORMS:'http://localhost:3001/platforms',
+    // SETUP:'http://localhost:3001/setup',
+    // IMAGES:'http://localhost:3001/images/',
+    URL: 'https://videogames-diegolepore.onrender.com',
+    VIDEOGAMES: `${URL}/videogames`,
+    GENRES: `${URL}/genres`,
+    PLATFORMS: `${URL}/platforms`,
+    SETUP: `${URL}/setup`,
+    IMAGES: `${URL}/images/`,
+
     LANDING: '/',
     HOME: '/home',
     FORM: '/form',
     FORM_EDIT: '/form/:id',
     PATHFORM_EDIT: '/form/',
-    SELECT_FILTERS:'/filters/select',
-    SELECT_ORDERS:'/orders/select',
+    SELECT_FILTERS: '/filters/select',
+    SELECT_ORDERS: '/orders/select',
     DETAILS: '/details/:id',
-    DETAIL:'/details/',
+    DETAIL: '/details/',
     FORMSETUP: '/setup',
-    
-    
+
+
 };
 export default SITEROUTES
